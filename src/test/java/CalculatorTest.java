10fe6ed81d6d22d5b12db79cd8cd7fb8deea02df
nzneil2 <33484241+nzneil2@users.noreply.github.com>
2019-04-08 15:40:48 +0200

    Update CalculatorTest.java
    Changed some values for different tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -18,35 +18,35 @@ public class CalculatorTest{
 
     @Test
     public void test_addPositiveNumbers_shouldBeAdded(){
-        int x = 2, y = 7;
+        int x = 10, y = 5;
 
         Assert.assertEquals(x+y, Calculator.add(x, y));
     }
 
     @Test
     public void test_addNegativeNumbers_shouldBeAdded(){
-        int x = -2, y = -7;
+        int x = -5, y = -10;
 
         Assert.assertEquals(x+y, Calculator.add(x, y));
     }
 
     @Test
     public void test_subtractPositiveNumbers_shouldBeSubtracted(){
-        int x = 7, y = 2;
+        int x = 9, y = 20;
 
         Assert.assertEquals(x-y, Calculator.subtract(x, y));
     }
 
     @Test
     public void test_subtractNegativeNumbers_shouldBeAdded(){
-        int x = -7, y = -2;
+        int x = -1, y = -4;
 
         Assert.assertEquals(x-y, Calculator.subtract(x, y));
     }
 
     @Test
     public void test_subtractLargerNumberFromSmallerNumber_shouldBeSubtracted(){
-        int x = -2, y = -7;
+        int x = -4, y = -2;
 
         Assert.assertEquals(x-y, Calculator.subtract(x, y));
     }
