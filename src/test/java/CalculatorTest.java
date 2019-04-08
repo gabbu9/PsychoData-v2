2c39d3f3794e34698201e2e6acd76b5cc08eb00b
nzneil2 <33484241+nzneil2@users.noreply.github.com>
2019-04-08 15:43:15 +0200

    Update CalculatorTest.java
    Hard coded numbers for better accuracy

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -20,34 +20,34 @@ public class CalculatorTest{
     public void test_addPositiveNumbers_shouldBeAdded(){
         int x = 10, y = 5;
 
-        Assert.assertEquals(x+y, Calculator.add(x, y));
+        Assert.assertEquals((10)+(5), Calculator.add(x, y));
     }
 
     @Test
     public void test_addNegativeNumbers_shouldBeAdded(){
         int x = -5, y = -10;
 
-        Assert.assertEquals(x+y, Calculator.add(x, y));
+        Assert.assertEquals((-5)+(10), Calculator.add(x, y));
     }
 
     @Test
     public void test_subtractPositiveNumbers_shouldBeSubtracted(){
         int x = 9, y = 20;
 
-        Assert.assertEquals(x-y, Calculator.subtract(x, y));
+        Assert.assertEquals((9)-(20), Calculator.subtract(x, y));
     }
 
     @Test
     public void test_subtractNegativeNumbers_shouldBeAdded(){
         int x = -1, y = -4;
 
-        Assert.assertEquals(x-y, Calculator.subtract(x, y));
+        Assert.assertEquals((-1)-(-4), Calculator.subtract(x, y));
     }
 
     @Test
     public void test_subtractLargerNumberFromSmallerNumber_shouldBeSubtracted(){
         int x = -4, y = -2;
 
-        Assert.assertEquals(x-y, Calculator.subtract(x, y));
+        Assert.assertEquals((-4)-(-2), Calculator.subtract(x, y));
     }
 }
