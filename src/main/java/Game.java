0147ee854355a37552f548817f30759c3e4e7522
nzneil2 <33484241+nzneil2@users.noreply.github.com>
2019-04-16 11:02:04 +0200

    Added generateHTMLFiles method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,13 @@
 import java.util.*;
+import java.io.*;
 public class Game{
     static Scanner in = new Scanner(System.in);
     int turns;
     static Player players[];
     Map map;
-    public static void main(String args[]){
+
+    public Game(){
+        System.out.print("Enter number of players: ");
         int playerCount = in.nextInt();
         do{
             if(setNumPlayers(playerCount)){
@@ -12,12 +15,13 @@ public class Game{
             }
             else{
                 System.out.println("Player Count needs to be between 2 and 8");
+                System.out.print("Enter number of players: ");
                 playerCount = in.nextInt();
             }
         }while(!setNumPlayers(playerCount));
+        generateHTMLFiles();
     }
-    public void startGame(){
-    }
+
     public static boolean setNumPlayers(int n){
         if(n>=2 && n<=8){
             return true;
@@ -26,6 +30,19 @@ public class Game{
             return false;
         }
     }
+
     public void generateHTMLFiles(){
+        File f = new File("htmlFile.html");
+        String toWrite = "<html><table border=\"1\"><tr><th>1H</th><th>2H</th><th>3H</th></tr><tr>"+
+                         "<td>1</td><td>2</td><td>3</td></tr><tr><td>4</td><td>5</td><td>6</td></tr><tr>"+
+                         "<td>9</td><td>8</td><td>7</td></tr></table></html>";
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+            bw.write(toWrite);
+            bw.close();
+        }catch(IOException io){
+            io.printStackTrace();
+        }
+        System.out.println("Wrote to file");
     }
 }
