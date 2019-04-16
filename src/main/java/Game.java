69763584e833f92dc02517c911b03b74e3f43e71
gabbu9 <33454400+gabbu9@users.noreply.github.com>
2019-04-16 12:48:18 +0200

    Update Game.java
    Made modifications to how table HTML code was generated, as well as some changes to the main method

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,22 +4,22 @@ public class Game{
     static Scanner in = new Scanner(System.in);
     int turns;
     static Player players[];
-    Map map;
-
-    public Game(){
-        System.out.print("Enter number of players: ");
+    static MyMap map;
+    public static void main(String args[]){
+        System.out.println("Enter Player Count");
         int playerCount = in.nextInt();
         do{
-            if(setNumPlayers(playerCount)){
-                players = new Player[playerCount];
-            }
-            else{
-                System.out.println("Player Count needs to be between 2 and 8");
-                System.out.print("Enter number of players: ");
+            if(!setNumPlayers(playerCount)){
+                System.out.println("Player Count needs to be between 2 and 8\n\nEnter Player Count");
                 playerCount = in.nextInt();
             }
         }while(!setNumPlayers(playerCount));
+        players = new Player[playerCount];
+        map = new MyMap(playerCount);
         generateHTMLFiles();
+    }
+
+    public void startGame(){
     }
 
     public static boolean setNumPlayers(int n){
@@ -31,11 +31,28 @@ public class Game{
         }
     }
 
-    public void generateHTMLFiles(){
+    public static void generateHTMLFiles(){
         File f = new File("htmlFile.html");
-        String toWrite = "<html><table border=\"1\"><tr><th>1H</th><th>2H</th><th>3H</th></tr><tr>"+
-                         "<td>1</td><td>2</td><td>3</td></tr><tr><td>4</td><td>5</td><td>6</td></tr><tr>"+
-                         "<td>9</td><td>8</td><td>7</td></tr></table></html>";
+        StringBuilder table = new StringBuilder();
+        String ROW_START = "<tr>";
+        String ROW_END = "</tr>";
+        String COLUMN_START = "<td>";
+        String COLUMN_END = "</td>";
+
+        for(int i = 0; i < MyMap.getSize(); i++){
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(ROW_START);
+            for(int j = 0; j < MyMap.getSize(); j++){
+                sb.append(COLUMN_START);
+                sb.append(MyMap.getTileType(i,j));
+                sb.append(COLUMN_END);
+
+            }
+            sb.append(ROW_END);
+            table.append(sb.toString());
+        }
+        String toWrite = "<html><table border=\"1\">"+table.toString()+"</table></html>";
         try{
             BufferedWriter bw = new BufferedWriter(new FileWriter(f));
             bw.write(toWrite);
