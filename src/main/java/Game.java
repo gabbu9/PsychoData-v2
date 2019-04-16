1daaa990e626cae34bf129ba6a9363e6a8e90b78
nzneil2 <33484241+nzneil2@users.noreply.github.com>
2019-04-16 12:56:52 +0200

    Added color functionality to game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,7 +5,7 @@ public class Game{
     int turns;
     static Player players[];
     static MyMap map;
-    public static void main(String args[]){
+   public static void main(String args[]){
         System.out.println("Enter Player Count");
         int playerCount = in.nextInt();
         do{
@@ -18,6 +18,7 @@ public class Game{
         map = new MyMap(playerCount);
         generateHTMLFiles();
     }
+
 
     public void startGame(){
     }
@@ -36,23 +37,24 @@ public class Game{
         StringBuilder table = new StringBuilder();
         String ROW_START = "<tr>";
         String ROW_END = "</tr>";
-        String COLUMN_START = "<td>";
-        String COLUMN_END = "</td>";
-
+        String COLUMN_START = "<td";
+        String COLUMN_END = "></td>";
         for(int i = 0; i < MyMap.getSize(); i++){
             StringBuilder sb = new StringBuilder();
-
             sb.append(ROW_START);
             for(int j = 0; j < MyMap.getSize(); j++){
                 sb.append(COLUMN_START);
+                if(MyMap.getTileType(i,j)=='w')sb.append(" class=\"tg-2n01\"");
+                else if(MyMap.getTileType(i,j)=='g')sb.append(" class=\"tg-d52n\"");
+                else if(MyMap.getTileType(i,j)=='y')sb.append(" class=\"tg-kusv\"");
                 sb.append(MyMap.getTileType(i,j));
                 sb.append(COLUMN_END);
-
             }
             sb.append(ROW_END);
             table.append(sb.toString());
         }
-        String toWrite = "<html><table border=\"1\">"+table.toString()+"</table></html>";
+        String style = "<style type=\"text/css\">.tg  {border-collapse:collapse;border-spacing:0;}.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}.tg .tg-kusv{background-color:#fffe65;border-color:inherit;text-align:left;vertical-align:top}.tg .tg-2n01{background-color:#3531ff;border-color:inherit;text-align:left;vertical-align:top}.tg .tg-d52n{background-color:#32cb00;border-color:inherit;text-align:left;vertical-align:top}.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}</style>";
+        String toWrite = style+"<table class=\"tg\">"+table.toString()+"</table>";
         try{
             BufferedWriter bw = new BufferedWriter(new FileWriter(f));
             bw.write(toWrite);
