e5d4acc5105a079e3eff11b8d13972bb8b9499e7
gabbu9 <33454400+gabbu9@users.noreply.github.com>
2019-04-16 18:35:29 +0200

    Update Player.java
    Changed vector to boolean 2d array for keeping track of previously discovered tiles

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -6,8 +6,7 @@ public class Player{
     private static int randX = 0, randY = 0;   
     private static int maxSize;
     private Position pos = new Position();
-    private Vector<Position> visited = new Vector<Position>();
-
+    private boolean[][] visited;
     public Player(MyMap currMap){
         do{
             randX = (int)(Math.random()*currMap.getSize());
@@ -16,8 +15,9 @@ public class Player{
         System.out.println("Start Position: ("+randX+","+randY+")");
         pos.setX(randX);
         pos.setY(randY);
-        visited.add(pos);
         this.maxSize = currMap.getSize();
+        visited = new boolean[currMap.getSize()][currMap.getSize()];
+        visited[pos.getX()][pos.getY()] = true;
     }//not sure if you can generate multiple players
 
     public void move(char direction){ //updating player position
@@ -41,7 +41,7 @@ public class Player{
             System.out.println("Invalid direction");
             return;
         }
-        visited.add(pos);
+        visited[pos.getX()][pos.getY()] = true;
         System.out.println("Moved to: ("+pos.getX()+","+pos.getY()+")");
     }
 
@@ -69,14 +69,13 @@ public class Player{
         pos.setY(y);
     }
     
-    public Vector<Position> getVisited(){
-        return visited;
+    public boolean getVisited(int i,int j){
+        return visited[i][j];
     }
 
     /*public void draw(Graphics2D g2d){
     g2d.drawImage(getPlayerImg(),x,y,null);
     }
-
     public Image getPlayerImg() {
     ImageIcon ic = new ImageIcon();//upload player image here
     return ic.getImage();
