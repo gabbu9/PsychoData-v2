1381a98ca28b664c186c8d8984c316c4d62a6d1c
nzneil2 <33484241+nzneil2@users.noreply.github.com>
2019-04-16 13:43:37 +0200

    Added position.java class

---
+++ src/main/java/Position.java
@@ -0,0 +1,22 @@
+public class Position{
+    private int x;
+    private int y;
+    
+    public Position(){}
+    
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public void setY(int y){
+        this.y = y;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+}
