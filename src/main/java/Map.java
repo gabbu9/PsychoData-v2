0e29bd9fa7a4a0f6f3e6e65c6b968005c3fa90b9
gabbu9 <33454400+gabbu9@users.noreply.github.com>
2019-04-16 12:49:15 +0200

    Update Map.java
    Removed displaying of map tiles in java and instead map can be viewed through generated HTML

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
-class Map{
-    private char[][] Map;
-    private int size;
-    public Map(int players){
+class MyMap{
+    private static char[][] MyMap;
+    private static int size;
+    public MyMap(int players){
         System.out.print('\u000C');
         if(players>=2&&players<=4){
             size = (int)(Math.random()*45)+5;
@@ -12,20 +12,14 @@ class Map{
         if(setMapSize(size,size)){
             generate();
             System.out.println("Generated Map with size: "+size+" x "+size);
-            for(int i = 0;i<size;i++){
-                for (int j = 0; j<size; j++){
-                    System.out.print(getTileType(i,j)+" ");
-                }
-                System.out.print("\n");
-            }
         }
     }
     public boolean setMapSize(int x, int y){
         if(x<=50&&y<=50&&x>=5&&y>=5){
-            Map = new char[x][y];
+            MyMap = new char[x][y];
             for(int i = 0;i<size;i++){
                 for (int j = 0; j<size; j++){
-                    Map[i][j] = 'g';
+                    MyMap[i][j] = 'g';
                 }
             }
             return true;
@@ -33,11 +27,17 @@ class Map{
     }
     public void generate(){
         for(int i = 0; i<(int)size*1.5; i++){
-            Map[(int)(Math.random()*size)][(int)(Math.random()*size)]='w';
+            MyMap[(int)(Math.random()*size)][(int)(Math.random()*size)]='w';
         }
-        Map[(int)(Math.random()*size)][(int)(Math.random()*size)]='y';
+        MyMap[(int)(Math.random()*size)][(int)(Math.random()*size)]='y';
     }
-    public char getTileType(int x, int y){
-        return Map[x][y];
+    public static char getTileType(int x, int y){
+        return MyMap[x][y];
+    }
+    public static int getSize(){
+        return size;
+    }
+    public static char[] getMyMap(int i){
+        return MyMap[i];
     }
 }
